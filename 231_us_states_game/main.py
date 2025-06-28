382b3d5b2c80e946c14dfc0f4a7a4c3c149c4319
ezid007 <junginhoe@gmail.com>
2025-06-28 13:06:41 +0900

    2025/06/28_학원 데탑 연동

---
+++ 231_us_states_game/main.py
@@ -0,0 +1,17 @@
+import turtle
+
+IMAGE = "self_study/231_us_states_game/blank_states_img.gif"
+
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
+
+def get_mouse_click_coor(x, y):
+    print(x,y)
+
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
+
+screen.exitonclick()
