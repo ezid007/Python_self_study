794b5d3afd335aea4cf9356e19c7cc5e42bb632a
junginhoe <junginhoe@gmail.com>
2025-06-30 08:37:54 +0900

    6월 30일 프로그래밍 시작

--- Day_22_231_us_states_game/main.py
+++ Day_22_231_us_states_game/main.py
@@ -1,5 +1,9 @@
+from rich import print
 import turtle
 import pandas as pd
+import os
+
+os.system('cls')
 
 IMAGE = "self_study/Day_22_231_us_states_game/blank_states_img.gif"
 STATES_50 = "self_study/Day_22_231_us_states_game/50_states.csv"
@@ -17,18 +21,22 @@ turtle.shape(IMAGE)
 
 data = pd.read_csv(STATES_50)
 all_states = data.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(
-    title="Guess the State", prompt="What's another state's name?"
-)
+while len(guessed_states) < 50:
+    
+    answer_state = screen.textinput(
+        title="Guess the State", prompt="What's another state's name?"
+    )
 
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(state_data.x, state_data.y)
-    t.color("blue")
-    t.stamp()
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x.item()), int(state_data.y.item()))
+        t.write(state_data.state.iloc[0])
 
 screen.exitonclick()
+
