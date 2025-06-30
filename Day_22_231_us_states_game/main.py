72520a669f2cd09afaeb75ef455bec84aa097db4
junginhoe <junginhoe@gmail.com>
2025-06-30 12:43:10 +0900

    미국 지역  맞추기 게임 완성

--- Day_22_231_us_states_game/main.py
+++ Day_22_231_us_states_game/main.py
@@ -1,17 +1,17 @@
 from rich import print
 import turtle
 import pandas as pd
-import os
-
-os.system('cls')
 
 IMAGE = "self_study/Day_22_231_us_states_game/blank_states_img.gif"
-STATES_50 = "self_study/Day_22_231_us_states_game/50_states.csv"
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
+
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
 
 # def get_mouse_click_coor(x, y):
 #     print(x,y)
@@ -19,24 +19,30 @@ turtle.shape(IMAGE)
 # turtle.onscreenclick(get_mouse_click_coor)
 # turtle.mainloop()
 
-data = pd.read_csv(STATES_50)
+data = pd.read_csv("self_study/Day_22_231_us_states_game/50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    
     answer_state = screen.textinput(
-        title="Guess the State", prompt="What's another state's name?"
+        title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?"
     )
 
-    if answer_state in all_states:
+    if answer_state is None:
+        continue
+    answer_state = answer_state.title()
+
+    if answer_state == 'Exit':
+        missing_states = [state for state in all_states if state not in guessed_states]
+        pd.DataFrame(missing_states).to_csv("self_study/Day_22_231_us_states_game/states_to_learn.csv")
+        break
+
+    if answer_state in all_states and answer_state not in guessed_states:
         guessed_states.append(answer_state)
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
         state_data = data[data.state == answer_state]
-        t.goto(int(state_data.x.item()), int(state_data.y.item()))
-        t.write(state_data.state.iloc[0])
-
-screen.exitonclick()
-
+        
+        x_cor = state_data.x.item()
+        y_cor = state_data.y.item()
+        t.goto(x_cor, y_cor)
+        
+        t.write(state_data.state.item())
