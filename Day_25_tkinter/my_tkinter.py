52107443ffe4a64b866e8a763b7afaa9f109da86
junginhoe <junginhoe@gmail.com>
2025-07-03 10:54:31 +0900

    뽀모도로 기법 만들기 시작

--- Day_25_tkinter/my_tkinter.py
+++ Day_25_tkinter/my_tkinter.py
@@ -8,8 +8,6 @@ window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="I an a Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
 my_label['text'] = "New text"
 
 button = tkinter.Button(text="Click Me", command=button_clicked)
