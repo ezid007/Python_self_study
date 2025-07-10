fff203d2660ed09c8fc6e2ffe5020d1d8851f4e8
junginhoe <junginhoe@gmail.com>
2025-07-10 17:28:42 +0900

    kanye_rest 시작

---
+++ Day_37_300_kanye/kanye_rest.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import requests
+
+
+def get_quote():
+    kanye_rest = requests.get("https://api.kanye.rest/")
+    kanye_rest.raise_for_status()
+    data = kanye_rest.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="self_study\\Day_37_300_kanye\\background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(
+    150,
+    207,
+    text="Kanye Quote Goes HERE",
+    width=250,
+    font=("Arial", 20, "bold"),
+    fill="white",
+)
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="self_study\\Day_37_300_kanye\\kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+window.mainloop()
